9e4ab776fd5caa3880b3047ac0e92d7a1d854cdc
AbdullahBasem <abdullah.basem.j@gmail.com>
2023-11-26 13:29:13 +0300

    [add] coming soon  page [add] coming soon page assets

---
+++ resources/views/coming_soon.blade.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <title>Dopamine Tech -Triple F</title>
+    <meta content="" name="description">
+    <meta content="" name="keywords">
+    <link href="{{asset('coming_soon_assets/assets/img/favicon.png')}}" rel="icon">
+    <link href="{{asset('coming_soon_assets/assets/img/apple-touch-icon.png')}}" rel="apple-touch-icon">
+    <link
+        href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,600,600i,700,700i"
+        rel="stylesheet">
+    <link href="{{asset('coming_soon_assets/assets/vendor/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
+    <link href="{{asset('coming_soon_assets/assets/vendor/bootstrap-icons/bootstrap-icons.css')}}" rel="stylesheet">
+    <link href="{{asset('coming_soon_assets/assets/css/style.css')}}" rel="stylesheet">
+</head>
+<body>
+<header id="header">
+    <div class="container-fluid d-flex justify-content-between align-items-center">
+        <div class="logo">
+            <h1 class="text-light"><a href="index.html"><span>Triple F</span></a></h1>
+        </div>
+    </div>
+</header>
+<section id="hero">
+    <div class="hero-container">
+        <h1>Triple F By Dopamine Technology</h1>
+        <h2>We're working hard to improve our website and we'll ready to launch after</h2>
+        <div class="countdown" data-count="2023/12/3" data-template="%d days %h:%m:%s">60 10:48:32</div>
+    </div>
+</section>
+
+<footer id="footer">
+    <div class="container">
+        <div class="copyright">
+            &copy; Copyright <strong><span>Dopamine Technology</span></strong>. All Rights Reserved
+        </div>
+        <div class="credits">
+            Designed by <a href="#">Dopamine Technology</a>
+        </div>
+    </div>
+</footer>
+<script src="{{asset('coming_soon_assets/assets/vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+<script src="{{asset('coming_soon_assets/assets/vendor/php-email-form/validate.js')}}"></script>
+<script src="{{asset('coming_soon_assets/assets/js/main.js')}}"></script>
+</body>
+
+</html>
