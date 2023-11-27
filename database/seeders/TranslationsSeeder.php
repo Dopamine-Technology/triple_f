0cde1c96678c2396dad82c0a6b3e4f17370552a9
AbdullahBasem <abdullah.basem.j@gmail.com>
2023-11-27 10:57:03 +0300

    [fix] translations seeder

--- database/seeders/TranslationsSeeder.php
+++ database/seeders/TranslationsSeeder.php
@@ -15,7 +15,7 @@ class TranslationsSeeder extends Seeder
     {
         $loremText = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
         $translations = array();
-        $translations[] = ['key' => 'hero_title', 'name' => json_encode(['en' => $loremText])];
+        $translations[] = ['key' => 'hero_title', 'value' => json_encode(['en' => $loremText])];
         Translation::query()->insert($translations);
     }
 }
