0be9d6d6d14f9d281f2c6f22a21e4ca7fd198b4a
abdullah basem <58694045+AbdullahBasem@users.noreply.github.com>
2024-05-07 15:53:31 +0300

    [add] forget password email [add] verify email [add] verify email api

--- app/Http/Resources/ChatResource.php
+++ app/Http/Resources/ChatResource.php
@@ -20,7 +20,7 @@ class ChatResource extends JsonResource
             'first_name' => $this->first_name,
             'last_name' => $this->last_name,
             'name' => $this->name,
-            'image' => $this->image ? 'https://acceptance-test.eu-central-1.linodeobjects.com' . $this->image : '',
+            'image' => $this->image ? 'https://acceptance-test.eu-central-1.linodeobjects.com/' . $this->image : '',
             'last_message' => MessageResource::make(
                 Message::query()->where(function ($query) {
                     $query->where('message_from', $this->id)
