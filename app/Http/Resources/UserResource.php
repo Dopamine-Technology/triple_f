6226d972ffc4d5558b7b82432f736ac97e0ae7c6
AbdullahBasem <abdullah.basem.j@gmail.com>
2024-05-15 11:15:06 +0300

    [add] country image to user resource

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -54,6 +54,7 @@ class UserResource extends JsonResource
                 'country' => $this->profile->country ? [
                     'id' => $this->profile->country->id,
                     'name' => $this->profile->country->getTranslation('name', LANGUAGE),
+                    'image'=> asset($this->image),
                 ] : null,
                 'city' => $this->profile->city ? [
                     'id' => $this->profile->city->id,
